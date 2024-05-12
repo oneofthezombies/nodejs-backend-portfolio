f6a97f0679b98ac61c33676c0e2b9cee7ae69e9f
oneofthezombies <hunhoekim@gmail.com>
2024-05-12 20:39:58 +0900

    build: add user table

---
+++ prisma/migrations/20240512113804_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
